8ee37196c98ebd752c760bcc4d93ebe088722a41
Seth Kellas <sjkellas@gmail.com>
2018-12-17 21:00:40 -0500

    Initial implementation of problem 001, get to green.

--- src/test/kotlin/com/skellas/euler/Problem001Test.kt
+++ src/test/kotlin/com/skellas/euler/Problem001Test.kt
@@ -1,4 +1,14 @@
 package com.skellas.euler
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 class Problem001Test {
+
+    @Test
+    fun shouldReturn23ForUnder10() {
+        val valueBelowTen = Problem001(0,10)
+        assertEquals(23, valueBelowTen.exec())
+    }
+
 }
